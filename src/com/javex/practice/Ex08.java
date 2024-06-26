f3283b8f0b698e87aff48f203d09c28577dc1a1a
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-26 18:01:56 +0900

    ex08

--- src/com/javex/practice/Ex08.java
+++ src/com/javex/practice/Ex08.java
@@ -3,6 +3,8 @@ package com.javex.practice;
 public class Ex08 {
 
 	public static void main(String[] args) {
+		//아래와 같이 구구단을 출력하세요
+
 		for (int i = 1; i < 10 ; i++)  {
 			for (int j = 2; j < 10 ; j++) {
 				System.out.print(j + "*" + i + "=" + (i*j) + "	");
