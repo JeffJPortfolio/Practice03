c71dc785741442263bad575d71326b9fdcbedc30
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-26 16:39:13 +0900

    Ex02

---
+++ src/com/javex/practice/Ex02.java
@@ -0,0 +1,17 @@
+package com.javex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 아래의 출력결과를 예상하여 작성하세요.
+		//** 4줄에 걸쳐서
+		int x, y ;
+		for (x=0 ; x < 4 ; x++) {
+			for(y=0 ; y < 2 ; y++) { 
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
