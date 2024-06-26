13b5e5fceb3bff41db369611910890b5e0c4e7e9
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-26 16:47:24 +0900

    Ex04

---
+++ src/com/javex/practice/Ex04.java
@@ -0,0 +1,17 @@
+package com.javex.practice;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		// 아래의 출력결과를 예상하여 작성하세요.
+		//1부터4 그리고 10에서 -2 해서 2까지
+		for (int i = 1; i <5 ;i++) {
+			System.out.println(i);
+		}
+		System.out.println("==================");		
+		for (int i = 10; i > 0  ;i = i-2) {
+			System.out.println(i);
+		}
+	}
+
+}
