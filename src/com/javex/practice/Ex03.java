bff2fcbdad4154f24456c55d6d5686c4d2646807
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-26 16:42:58 +0900

    Ex03

---
+++ src/com/javex/practice/Ex03.java
@@ -0,0 +1,20 @@
+package com.javex.practice;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// 아래의 출력결과를 예상하여 작성하세요.
+		/*
+		 * *3 개 4줄 다) 
+		 */
+		int x, y ;
+		for (x=0 ; x < 4 ; x++) {
+			for(y=2 ; y >= 0 ; y--) { 
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+
+}
