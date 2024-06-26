e75366e7860bc11f1271c66268567cf444241631
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-06-26 16:55:49 +0900

    Ex06

--- src/com/javex/practice/Ex06.java
+++ src/com/javex/practice/Ex06.java
@@ -4,7 +4,13 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int ans = 0;
+		while (ans < 100) {
+			if (ans%5 == 0 && ans%7 ==0  && ans != 0) {
+				System.out.println(ans);
+			}
+			ans +=5 ;
+		}
 	}
 
 }
